af2c0d4b7c6552c4b1ecdc65e62bc5db4137c93a
Farabi285 <vaiakash074@gmail.com>
2024-06-14 16:17:21 +0300

    Update npx10.js

--- scripts/commands/npx10.js
+++ scripts/commands/npx10.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/FaUTATo.mp4',
+      '"https://i.imgur.com/IneBEky.mp4",',
       { responseType: 'stream' }
     )
   ).data;
